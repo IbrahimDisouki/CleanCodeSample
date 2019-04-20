d96a1442925fcebe1bc9d3ccd65b11cf806d81fd
Ibrahim <ibrahimdisouki18@gmail.com>
2019-04-20 15:56:19 +0200

    - Delete google-services.json and Constants.kt files from git.

--- app/src/main/java/com/example/cleancodesample/application/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cleancodesample.application
-
-object Constants {
-    const val BASE_URL = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/"
-    const val FIREBASE_API_KEY = "YOUR_FIREBASE_PROJECT_API_KEY"
-}
